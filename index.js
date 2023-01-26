26e2a1625120fd07871d3cd445c7c5aa69601024
Zach Derish <zderish@gmail.com>
2023-01-26 17:43:51 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -9,26 +9,33 @@ let forecast = document.getElementById("forecast-el")
 getForecast.addEventListener("click", function() {
     // console.log(coor.value)
     // let coorSplit = coor.value.split(",") 
-
+    forecast.innerText = ""
     // call weather api given latitude and longitude
-    let coorlink = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip.value},US&appid=//`
-    let coor = ''
+    let coorlink = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip.value},US&appid=APIKEY`
     fetch(coorlink)
         .then(res => res.json())
-        // get data from json
+        // get location data from json
         .then(data => getLocation(data))
-    let link = `https://api.weather.gov/points/${coorSplit[0]},${coorSplit[1]}`
-    console.log(link)
-    fetch(link)
-            .then(res => res.json())
-            // get data from json
-            .then(data => locationForecast(data))
 })
 
-// get location based on coordinates
+// get location based on coordinates from geocoding API
+function getLocation(coor){
+    console.log(coor, coor.lat, coor.lon)
+    // display location name
+    let name = document.createElement("li")
+    name.innerText = coor.name + " Forecast:"
+    forecast.appendChild(name)
 
+    // create link to weather api
+    let link = `https://api.weather.gov/points/${coor.lat},${coor.lon}`
+    console.log(link)
+    fetch(link)
+        .then(res => res.json())
+        // get data from json
+        .then(data => locationForecast(data))
+}
 
-// get forecast from json
+// get forecast from weather json
 function locationForecast(location) {
     let url = location.properties.forecast
     console.log(url)
@@ -42,7 +49,6 @@ function renderForecast(element) {
     for(let i = 0; i < 14; i++){
         let li = document.createElement("li")
         let forecastItem = `${element.periods[i].name}: ${element.periods[i].detailedForecast}`
-        console.log(forecastItem)
         li.innerText = forecastItem
         forecast.appendChild(li)
     }
